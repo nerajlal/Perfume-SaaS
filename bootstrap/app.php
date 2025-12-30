25c20dc61ff3408e74502846d99aa4b0c9cd35cd
neraj <nerajnerajlal@gmail.com>
2025-12-30 10:21:36 +0530

    erors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,5 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, \Illuminate\Http\Request $request) {
+            return response()->view('nurah.errors.404', [], 404);
+        });
     })->create();
